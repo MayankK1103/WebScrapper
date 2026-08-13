f54decb6a52216c4978e7cda7eef24282cbc399a
Mayank Kumar <mayankkumar@Mayanks-MacBook-Air.local>
2026-08-13 13:00:08 -0700

    to_model func for decimal percision for price in db

--- book.py
+++ book.py
@@ -17,6 +17,13 @@ class BookModel(Base):
     __tablename__ = "books"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    title: Mapped[str] = mapped_column(String())
+    title: Mapped[str] = mapped_column(String(), unique=True)
     rating: Mapped[str] = mapped_column(String())
     price: Mapped[Decimal] = mapped_column(Numeric(10,2))
+
+
+def to_model(book: Book) -> BookModel:
+    price = Decimal(book.price[1:])
+    bookModel =  BookModel(title = book.title, rating = book.rating, price = price)
+    return bookModel
+
--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from sqlalchemy import text
+from sqlalchemy.orm import Session
+from sqlalchemy import select
 from scraper import BookScraper
 from config import engine
+from book import Base, to_model, BookModel
 
 if __name__ == "__main__":
+    Base.metadata.create_all(engine)
     bs = BookScraper("https://books.toscrape.com/")
-    print(bs.book_list())
+
+    with Session(engine) as session:
+        statement = select(BookModel.title)
+        existing_books = session.scalars(statement).all()
+
+    model_instances = []
+    for item in bs.book_list():
+        if item.title not in existing_books:
+            model_instances.append(to_model(item))
+
+    with Session(engine) as session:
+        session.add_all(model_instances)
+        session.commit()
+        print()
+
+    
